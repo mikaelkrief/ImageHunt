550743dadc8fee13d4b769565d37835009264222
nimbusparis <mandrillon@gmail.com>
2018-03-13 16:18:02 +0100

    Add database migration at each startup

--- ImageHunt/Startup.cs
+++ ImageHunt/Startup.cs
@@ -52,12 +52,13 @@ namespace ImageHunt
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, HuntContext context)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
+      context.Database.Migrate();
       app.Use(async (context, next) =>
       {
         await next();
