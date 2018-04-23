ab405f44a821e93b948937af99e1ce9ee6a06002
Tony Nguyen <ngn.tony@gmail.com>
2018-04-23 15:39:32 +0200

    Fix create db script

--- ImageHunt/create_db.sql
+++ ImageHunt/create_db.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA ImageHunt;
 create USER 'HuntUser'@'localhost'
-  IDENTIFIED  by 'jhns_U4Has9'
+  IDENTIFIED  by 'jhns_U4Has9';
 
 GRANT ALL PRIVILEGES on ImageHunt.* to'HuntUser'@'localhost';
