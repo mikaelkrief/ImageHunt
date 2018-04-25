91c91b7fc0b15be5797bdd61b82cafa26c6dfecd
nimbusparis <mandrillon@gmail.com>
2018-04-25 13:59:28 +0200

    * Cleanup modules

--- ImageHunt/src/app/app.module.ts
+++ ImageHunt/src/app/app.module.ts
@@ -1,54 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { LocalStorageModule } from 'angular-2-local-storage';
-import { AppComponent } from './app.component';
-import { BsDropdownModule, ModalModule, TabsModule } from 'ngx-bootstrap';
-import {AlertModule } from 'ngx-bootstrap/alert';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { LocalStorageModule } from "angular-2-local-storage";
+import { AppComponent } from "./app.component";
+import { BsDropdownModule, ModalModule, TabsModule } from "ngx-bootstrap";
+import { AlertModule } from "ngx-bootstrap/alert";
 
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpModule } from '@angular/http';
-import { Ng2UiAuthModule, CustomConfig } from 'ng2-ui-auth';
-import { HomeModule } from '../home/home.module';
-import { PageNotFoundModule } from '../page-not-found/page.not.found.module';
-import { TeamModule } from '../team/team.module';
-import { PlayerModule } from '../player/player.module';
-import { TeamListComponent } from '../team/team-list/team-list.component';
-import { HomeComponent } from '../home/home.component';
-import { PageNotFoundComponent } from '../page-not-found/page.not.found.component';
-import { NavmenuModule } from '../navmenu/navmenu.module';
-import {AdminListModule} from '../admin/admin-list/admin-list.module';
-import {AdminListComponent} from '../admin/admin-list/admin-list.component';
-import {GameModule} from '../game/game.module';
-import {GameListComponent} from '../game/game-list/game-list.component';
-import {GameDetailComponent} from '../game/game-detail/game.detail.component';
-import {environment} from '../environments/environment';
-import {Globals} from '../shared/globals';
-import { TeamDetailComponent } from '../team/team-detail/team.detail.component';
-import {MapModule} from '../map/map.module';
-import {MapDetail2Component} from '../map/map-detail2/map-detail2.component';
-import { ContextMenuModule } from 'primeng/primeng';
-import {NodeRelationComponent} from '../game/node-relation/node.relation.component';
-import {NodeCreateComponent} from '../game/node-create/node.create.component';
-import { QuestionNodeComponent } from '../game/question-node/question.node.component';
-import {GameActionListComponent} from '../game/game-action-list/game-action-list.component';
-import {GameActionDetailComponent} from '../game/game-action-detail/game-action-detail.component';
-import localeFr from '@angular/common/locales/fr';
-import { registerLocaleData } from '@angular/common';
-import {SharedModule} from '../shared/shared.module';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpModule } from "@angular/http";
+import { Ng2UiAuthModule, CustomConfig } from "ng2-ui-auth";
+import { HomeModule } from "../home/home.module";
+import { PageNotFoundModule } from "../page-not-found/page.not.found.module";
+import { TeamModule } from "../team/team.module";
+import { PlayerModule } from "../player/player.module";
+import { TeamListComponent } from "../team/team-list/team-list.component";
+import { HomeComponent } from "../home/home.component";
+import { PageNotFoundComponent } from "../page-not-found/page.not.found.component";
+import { NavmenuModule } from "../navmenu/navmenu.module";
+import { AdminListModule } from "../admin/admin-list/admin-list.module";
+import { AdminListComponent } from "../admin/admin-list/admin-list.component";
+import { GameModule } from "../game/game.module";
+import { GameListComponent } from "../game/game-list/game-list.component";
+import { GameDetailComponent } from "../game/game-detail/game.detail.component";
+import { environment } from "../environments/environment";
+import { Globals } from "../shared/globals";
+import { TeamDetailComponent } from "../team/team-detail/team.detail.component";
+import { MapModule } from "../map/map.module";
+import { MapDetail2Component } from "../map/map-detail2/map-detail2.component";
+import { ContextMenuModule } from "primeng/primeng";
+import { NodeRelationComponent } from "../game/node-relation/node.relation.component";
+import { NodeCreateComponent } from "../game/node-create/node.create.component";
+import { QuestionNodeComponent } from "../game/question-node/question.node.component";
+import { GameActionListComponent } from "../game/game-action-list/game-action-list.component";
+import { GameActionDetailComponent } from "../game/game-action-detail/game-action-detail.component";
+import localeFr from "@angular/common/locales/fr";
+import { registerLocaleData } from "@angular/common";
+import { SharedModule } from "../shared/shared.module";
 
 registerLocaleData(localeFr);
 
 export class MyAuthConfig extends CustomConfig {
-  defaultHeaders = { 'Content-Type': 'application/json' };
+  defaultHeaders = { 'Content-Type': "application/json" };
   providers = {
     google: { clientId: environment.GOOGLE_CLIENT_ID },
   };
-  tokenName = 'accessToken';
-  tokenPrefix = '';
+  tokenName = "accessToken";
+  tokenPrefix = "";
   baseUrl = environment.API_ENDPOINT;
 }
+
 @NgModule({
   declarations: [
     AppComponent
@@ -58,8 +59,8 @@ export class MyAuthConfig extends CustomConfig {
     BrowserModule,
     BrowserAnimationsModule,
     LocalStorageModule.withConfig({
-      prefix: 'Img-Hunt',
-      storageType: 'localStorage'
+      prefix: "Img-Hunt",
+      storageType: "localStorage"
     }),
     FormsModule,
     HttpModule,
@@ -78,19 +79,19 @@ export class MyAuthConfig extends CustomConfig {
     AlertModule.forRoot(),
     TabsModule.forRoot(),
     ModalModule.forRoot(),
-    // TimepickerModule.forRoot(),
     RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
-      { path: 'game', component: GameListComponent },
-      { path: 'game/:gameId', component: GameDetailComponent },
-      { path: 'action/:gameId', component: GameActionListComponent},
-      { path: 'action/detail/:gameActionId', component: GameActionDetailComponent},
-      { path: 'team/:teamId', component: TeamDetailComponent },
-      { path: 'team', component: TeamListComponent },
-      { path: 'map', component: MapDetail2Component },
-      { path: 'admin', component: AdminListComponent },
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent }])
+      { path: "home", component: HomeComponent },
+      { path: "game", component: GameListComponent },
+      { path: "game/:gameId", component: GameDetailComponent },
+      { path: "action/:gameId", component: GameActionListComponent },
+      { path: "action/detail/:gameActionId", component: GameActionDetailComponent },
+      { path: "team/:teamId", component: TeamDetailComponent },
+      { path: "team", component: TeamListComponent },
+      { path: "map", component: MapDetail2Component },
+      { path: "admin", component: AdminListComponent },
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "**", component: PageNotFoundComponent }
+    ])
   ],
   providers: [Globals],
   entryComponents: [NodeCreateComponent, NodeRelationComponent, QuestionNodeComponent]
--- ImageHunt/src/map/map.module.ts
+++ ImageHunt/src/map/map.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {AgmCoreModule} from "@agm/core";
 import {FormsModule} from "@angular/forms";
 import {environment} from "../environments/environment";
 import {MapThumbnailComponent} from "./map-thumbnail/map.thumbnail.component";
@@ -9,7 +8,6 @@ import {MapDetail2Component} from "./map-detail2/map-detail2.component";
 
 @NgModule({
   imports: [CommonModule,
-    AgmCoreModule.forRoot({ apiKey: environment.GOOGLE_MAP_API_KEY }),
     FormsModule, GMapModule],
   declarations: [MapDetail2Component, MapThumbnailComponent],
   exports: [MapDetail2Component, MapThumbnailComponent]})
