2ab4cf240bdc64389a0734ec68cc57eea3b0b63b
nimbusparis <mandrillon@gmail.com>
2018-05-03 21:54:05 +0200

    * Fix dependencies of test projects * Add TeamWebService * Factorize test utilities

---
+++ TestUtilities/WebServiceBaseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using FakeItEasy;
+
+namespace TestUtilities
+{
+  public class WebServiceBaseTest : BaseTest
+  {
+    protected HttpMessageHandler _fakeHttpMessageHandler;
+    protected HttpClient _httpClient;
+
+    public WebServiceBaseTest()
+    {
+      _fakeHttpMessageHandler = A.Fake<HttpMessageHandler>();
+      _httpClient = new HttpClient(_fakeHttpMessageHandler) { BaseAddress = new Uri("http://test.com") };
+    }
+
+    protected void FakeResponse(string resourceName)
+    {
+      var httpResponse = new HttpResponseMessage
+      {
+        Content = new StringContent(GetStringFromResource(Assembly.GetCallingAssembly(), 
+          resourceName
+        ))
+      };
+      A.CallTo(_fakeHttpMessageHandler)
+        .Where(x => x.Method.Name == "SendAsync")
+        .WithReturnType<Task<HttpResponseMessage>>()
+        .Returns(httpResponse);
+    }
+  }
+}
