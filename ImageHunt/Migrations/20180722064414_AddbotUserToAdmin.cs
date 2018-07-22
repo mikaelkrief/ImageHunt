25e04dcdb88d0963404d6d46cccb07ae8c1736ad
nimbusparis <mandrillon@gmail.com>
2018-07-22 15:55:15 +0200

    * Fix migration for bot token

--- ImageHunt/Migrations/20180722064414_AddbotUserToAdmin.cs
+++ ImageHunt/Migrations/20180722064414_AddbotUserToAdmin.cs
@@ -8,7 +8,7 @@ namespace ImageHunt.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
           migrationBuilder.InsertData("Admins",
-            new string[] {"Email", "ExpirationDate", "Name", "IsDeleted", "Token", "Role"},
+            new string[] {"Email", "ExpirationTokenDate", "Name", "IsDeleted", "Token", "Role"},
             new object[] {"imagehuntbot@bot.com", DateTime.Today.AddYears(1000), "ImageHuntBot", false, "ImageHuntBotToken", 4 });
         }
 
