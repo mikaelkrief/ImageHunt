64e51f7f07871abd88f46da55304e975c6fe0102
nimbusparis <mandrillon@gmail.com>
2018-07-26 15:27:41 +0200

    Fix misspelling

--- ImageHuntBot/Dialogs/HelpDialog.cs
+++ ImageHuntBot/Dialogs/HelpDialog.cs
@@ -15,7 +15,7 @@ namespace ImageHuntBot.Dialogs
         {
             LogInfo<ImageHuntState>(turnContext, "Begin HelpDialog");
             await turnContext.ReplyActivity(@"Pour jouer à la chasse au trésor, vous devez d'abord envoyer votre position. 
-Le plus simples est de fare un partage en continu le temps que dure la chasse.
+Le plus simples est de faire un partage en continu le temps que dure la chasse.
 Ensuite, vous pouvez envoyer les images que vous aurez deviné, je les enregistrerai et un validateur vous attribuera les points si elle correspond à une des photo mystère!
 Bon jeu!");
             await turnContext.End();
