e651b71cb1ba62a1ed59ff9e02c53b4775f424f1
nimbusparis <mandrillon@gmail.com>
2018-09-13 13:42:45 +0200

    * Add player penalty in Game model #IH-111

---
+++ ImageHunt/Migrations/20180913112550_AddPlayerPenalty.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImageHunt.Migrations
+{
+    public partial class AddPlayerPenalty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NbPlayerPenaltyThreshold",
+                table: "Games",
+                nullable: false,
+                defaultValue: 3);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NbPlayerPenaltyValue",
+                table: "Games",
+                nullable: false,
+                defaultValue: 0.05);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NbPlayerPenaltyThreshold",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "NbPlayerPenaltyValue",
+                table: "Games");
+        }
+    }
+}
