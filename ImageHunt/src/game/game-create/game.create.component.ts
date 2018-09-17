b7031df28db162353bf59a5fd47821bd98b96f84
nimbusparis <mandrillon@gmail.com>
2018-09-17 22:35:24 +0200

    * Create game in modal

--- ImageHunt/src/game/game-create/game.create.component.ts
+++ ImageHunt/src/game/game-create/game.create.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Globals} from "../../shared/globals";
 import {Admin} from "../../shared/admin";
+import { Game } from '../../shared/game';
+import { BsModalRef } from 'ngx-bootstrap';
 
 @Component({
     selector: 'game-create',
@@ -10,13 +12,33 @@ import {Admin} from "../../shared/admin";
 /** game.create component*/
 export class GameCreateComponent implements OnInit
 {
+  @Output() game = new EventEmitter<Game>();
   admin: Admin;
-  value: Date;
+  name: string;
+  startDate: string;
+  startTime: string;
     /** game.create ctor */
-    constructor(private globals:Globals) { }
+  constructor(private globals: Globals, public bsModalRef: BsModalRef) { }
 
     /** Called by Angular after game.create component initialized */
     ngOnInit(): void {
       this.admin = this.globals.connectedUser;
-    }
+  }
+  createGame() {
+    let game: Game = {
+      id: 0,
+      isActive: true,
+      name: this.name,
+      startDate: new Date(this.startDate + ' ' + this.startTime),
+      mapCenterLng: 0,
+      mapCenterLat: 0,
+      mapZoom: 1,
+      nodes: [],
+      teams: []
+    };
+    this.game.emit(game);
+    this.name = '';
+    this.startDate = undefined;
+    this.startTime = undefined;
+  }
 }
