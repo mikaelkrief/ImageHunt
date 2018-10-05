d2b7e2bf9494d60f1f0b3849a85feafd9e9fd1c5
nimbusparis <mandrillon@gmail.com>
2018-10-05 23:10:17 +0200

    * Bot Dialog to add new user in group to the team associated #IH-134

---
+++ ImageHuntBot/Dialogs/NewUserDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using ImageHuntTelegramBot;
+using ImageHuntTelegramBot.Dialogs;
+using Microsoft.Extensions.Logging;
+
+namespace ImageHuntBot.Dialogs
+{
+    public class NewUserDialog : AbstractDialog, INewUserDialog
+    {
+        public NewUserDialog(ILogger<NewUserDialog> logger) : base(logger)
+        {
+        }
+
+        public override async Task Begin(ITurnContext turnContext)
+        {
+            var state = turnContext.GetConversationState<ImageHuntState>();
+            try
+            {
+                if (state.GameId == 0 || state.TeamId == 0)
+                {
+                    _logger.LogError($"Unable to add a user to a team for a non-initialized group");
+                }
+            }
+            finally
+            {
+                await turnContext.End();
+            }
+        }
+
+        public override string Command => "/newUser";
+    }
+}
