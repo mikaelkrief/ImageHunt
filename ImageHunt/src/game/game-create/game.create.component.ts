aaac00aaea753524cd9cf317395da1803319613d
nimbusparis <mandrillon@gmail.com>
2018-10-08 21:39:43 +0200

    * fix build

--- ImageHunt/src/game/game-create/game.create.component.ts
+++ ImageHunt/src/game/game-create/game.create.component.ts
@@ -24,7 +24,7 @@ export class GameCreateComponent implements OnInit
     ngOnInit(): void {
       this.admin = this.globals.connectedUser;
   }
-  uploadImage() {
+  uploadPicture() {
 
   }
   createGame() {
