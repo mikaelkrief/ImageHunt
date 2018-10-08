ff7e6ee27f4f152686bf8995e9a43e0188e14d11
nimbusparis <mandrillon@gmail.com>
2018-10-08 20:57:11 +0200

    * Fix culture for date in InitDialog according to team culture #IH-135 fixed

--- ImageHuntWebServiceClient/Responses/TeamResponse.cs
+++ ImageHuntWebServiceClient/Responses/TeamResponse.cs
@@ -8,5 +8,6 @@ namespace ImageHuntWebServiceClient.Responses
         public PlayerResponse[] Players { get; set; }
         public int GameId { get; set; }
         public string ChatId { get; set; }
+        public string CultureInfo { get; set; }
     }
 }
