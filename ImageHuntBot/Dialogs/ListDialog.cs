1cdb10057f43122f7185ba2412e919d21360bd80
nimbusparis <mandrillon@gmail.com>
2018-10-24 18:26:23 +0200

    * Troll dialog #IH-143 fixed

---
+++ ImageHuntBot/Dialogs/ListDialog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageHuntTelegramBot;
+using ImageHuntTelegramBot.Dialogs;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types;
+
+namespace ImageHuntBot.Dialogs
+{
+    public class ListDialog: AbstractDialog, IListDialog
+
+    {
+        public ListDialog(ILogger<ListDialog> logger) : base(logger)
+        {
+        }
+
+        public async override Task Begin(ITurnContext turnContext)
+        {
+            try
+            {
+                var activity = new Activity(){ActivityType = ActivityType.Message, Text = "Veuillez vous rendre à la position ci-dessous", Location = new Location(){Latitude = 0, Longitude = 0}};
+                await turnContext.ReplyActivity(activity);
+            }
+            finally
+            {
+                await turnContext.End();
+            }
+        }
+
+        public override string Command => "/liste";
+    }
+}
