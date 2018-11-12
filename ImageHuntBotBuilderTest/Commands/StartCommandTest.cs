ff1951b4d1ff64e5461a27528a23b7ddbced7763
nimbusparis <mandrillon@gmail.com>
2018-11-12 22:06:13 +0100

    * Implements StartCommand

--- ImageHuntBotBuilderTest/Commands/StartCommandTest.cs
+++ ImageHuntBotBuilderTest/Commands/StartCommandTest.cs
@@ -7,6 +7,7 @@ using FakeItEasy;
 using ImageHuntBotBuilder;
 using ImageHuntBotBuilder.Commands;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 using TestUtilities;
 using Xunit;
@@ -16,33 +17,26 @@ namespace ImageHuntBotBuilderTest.Commands
     public class StartCommandTest : BaseTest<StartCommand>
     {
         private ILogger<IStartCommand> _logger;
+        private ImageHuntState _state;
+        private ITurnContext _turnContext;
 
         public StartCommandTest()
         {
             _testContainerBuilder.RegisterInstance(_logger = A.Fake<ILogger<IStartCommand>>());
+            _testContainerBuilder.RegisterInstance(A.Fake<IRedeemCommand>());
+            _turnContext = A.Fake<ITurnContext>();
             Build();
         }
 
         [Fact]
-        public void Should_StartCommand_Execute_RedeemCommand()
+        public async Task Should_StartCommand_Execute_RedeemCommand()
         {
             // Arrange
-            
+            var activity = new Activity(text: "/start redeem_gameId=21_pass=GFHFTF");
+            A.CallTo(() => _turnContext.Activity).Returns(activity);
             // Act
-
+            await _target.Execute(_turnContext, _state);
             // Assert
-        }
-    }
-    [Command("start")]
-    public class StartCommand : AbstractCommand, IStartCommand
-    {
-        public StartCommand(ILogger<IStartCommand> logger) : base(logger)
-        {
-        }
-
-        protected override Task InternalExecute(ITurnContext turnContext, ImageHuntState state)
-        {
-            throw new NotImplementedException();
         }
     }
 }
