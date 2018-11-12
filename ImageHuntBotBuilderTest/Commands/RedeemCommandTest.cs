af23a3b6690ebf1ccbc63b94371f831efe4b69f0
nimbusparis <mandrillon@gmail.com>
2018-11-12 21:45:57 +0100

    * Implements RedeemCommand * Implements StartCommand (TBC)

---
+++ ImageHuntBotBuilderTest/Commands/RedeemCommandTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using FakeItEasy;
+using ImageHuntBotBuilder;
+using ImageHuntBotBuilder.Commands;
+using ImageHuntWebServiceClient.WebServices;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using TestUtilities;
+using Xunit;
+
+namespace ImageHuntBotBuilderTest.Commands
+{
+    public class RedeemCommandTest : BaseTest<RedeemCommand>
+    {
+        private ILogger<IRedeemCommand> _logger;
+        private ITurnContext _turnContext;
+        private ImageHuntState _state;
+        private IActionWebService _actionWebService;
+        private IPasscodeWebService _passcodeWebService;
+
+        public RedeemCommandTest()
+        {
+            _testContainerBuilder.RegisterInstance(_logger = A.Fake<ILogger<IRedeemCommand>>());
+            _testContainerBuilder.RegisterInstance(_passcodeWebService = A.Fake<IPasscodeWebService>());
+            _testContainerBuilder.RegisterInstance(A.Fake<IBroadcastCommand>());
+            _turnContext = A.Fake<ITurnContext>();
+            _state = new ImageHuntState();
+            Build();
+        }
+
+        [Fact]
+        public async Task Should_Execute_Redeem_Passcode_For_Team()
+        {
+            // Arrange
+            var activity = new Activity(text: "/redeem gameid=45 pass=FGFGFFGT", from: new ChannelAccount(name:"toto"));
+            A.CallTo(() => _turnContext.Activity).Returns(activity);
+            // Act
+            await _target.Execute(_turnContext, _state);
+            // Assert
+            A.CallTo(() => _passcodeWebService.RedeemPasscode(45, "toto", "FGFGFFGT"));
+            A.CallTo(
+                    () => _turnContext.SendActivityAsync(A<string>._, A<string>._, A<string>._, A<CancellationToken>._))
+                .MustHaveHappened();
+        }
+    }
+}
---
+++ ImageHuntBotBuilderTest/Commands/StartCommandTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using FakeItEasy;
+using ImageHuntBotBuilder;
+using ImageHuntBotBuilder.Commands;
+using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Logging;
+using TestUtilities;
+using Xunit;
+
+namespace ImageHuntBotBuilderTest.Commands
+{
+    public class StartCommandTest : BaseTest<StartCommand>
+    {
+        private ILogger<IStartCommand> _logger;
+
+        public StartCommandTest()
+        {
+            _testContainerBuilder.RegisterInstance(_logger = A.Fake<ILogger<IStartCommand>>());
+            Build();
+        }
+
+        [Fact]
+        public void Should_StartCommand_Execute_RedeemCommand()
+        {
+            // Arrange
+            
+            // Act
+
+            // Assert
+        }
+    }
+    [Command("start")]
+    public class StartCommand : AbstractCommand, IStartCommand
+    {
+        public StartCommand(ILogger<IStartCommand> logger) : base(logger)
+        {
+        }
+
+        protected override Task InternalExecute(ITurnContext turnContext, ImageHuntState state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
