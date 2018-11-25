f895600e7d085c80bb8f454921e00b23ca3093b9
nimbusparis <mandrillon@gmail.com>
2018-11-25 09:12:01 +0100

    * Fix broadcast

---
+++ ImageHuntBotBuilderTest/ImageHuntActivityTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageHuntBotBuilder;
+using Microsoft.Bot.Schema;
+using NFluent;
+using TestUtilities;
+using Xunit;
+
+namespace ImageHuntBotBuilderTest
+{
+    public class ImageHuntActivityTest : BaseTest<ImageHuntActivity>
+    {
+        public ImageHuntActivityTest()
+        {
+            Build();
+        }
+
+        [Fact]
+        public void Should_Set_Conversation_If_Null()
+        {
+            // Arrange
+            var conversatonReference = new ConversationReference(){Conversation = new ConversationAccount()};
+            // Act
+            _target.ApplyConversationReference(conversatonReference);
+            // Assert
+            Check.That(_target.Conversation).Equals(conversatonReference.Conversation);
+        }
+        [Fact]
+        public void Should_Not_Set_Conversation_If_Not_Null()
+        {
+            // Arrange
+            _target.Conversation = new ConversationAccount();
+            var conversatonReference = new ConversationReference(){Conversation = new ConversationAccount()};
+            // Act
+            _target.ApplyConversationReference(conversatonReference);
+            // Assert
+            Check.That(_target.Conversation).IsNotEqualTo(conversatonReference.Conversation);
+        }
+    }
+}
---
+++ ImagehuntBotBuilder/ImageHuntActivity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Bot.Schema;
+
+namespace ImageHuntBotBuilder
+{
+    public class ImageHuntActivity : Activity
+    {
+        public new Activity ApplyConversationReference(ConversationReference reference, bool isIncoming = false)
+        {
+            this.ChannelId = reference.ChannelId;
+            this.ServiceUrl = reference.ServiceUrl;
+            if (this.Conversation == null)
+                this.Conversation = reference.Conversation;
+
+            if (isIncoming)
+            {
+                if (this.From == null)
+                    this.From = reference.User;
+                if (this.Recipient == null)
+                    this.Recipient = reference.Bot;
+                if (reference.ActivityId != null)
+                    this.Id = reference.ActivityId;
+            }
+            else // Outgoing
+            {
+                if (this.From == null)
+
+                    this.From = reference.Bot;
+                if (this.Recipient == null)
+
+                    this.Recipient = reference.User;
+                if (reference.ActivityId != null)
+                    this.ReplyToId = reference.ActivityId;
+            }
+
+            return this;
+        }
+    }
+}
