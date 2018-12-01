71a76afc11a9c498319246819c64a7500b7e9894
nimbusparis <mandrillon@gmail.com>
2018-12-01 21:06:02 +0100

    * Fix api call

--- ImageHuntWebServiceClient/WebServices/ImageWebService.cs
+++ ImageHuntWebServiceClient/WebServices/ImageWebService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.Extensions.Logging;
 
 namespace ImageHuntWebServiceClient.WebServices
@@ -17,9 +20,14 @@ namespace ImageHuntWebServiceClient.WebServices
         {
             using (var streamContent = new StreamContent(imageStream))
             {
-                var response = await _httpClient.PostAsync($"{_httpClient.BaseAddress}api/Team/", streamContent);
-                var idAdText = await response.Content.ReadAsStringAsync();
-                return Convert.ToInt32(idAdText);
+                var response = await _httpClient.PostAsync($"{_httpClient.BaseAddress}api/Image/", streamContent);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var idAdText = await response.Content.ReadAsStringAsync();
+                    return Convert.ToInt32(idAdText);
+                }
+
+                return 0;
             }
         }
     }
