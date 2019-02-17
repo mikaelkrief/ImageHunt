af7975984264e5b890581fbc62d05a0400352bf0
nimbusparis <mandrillon@gmail.com>
2019-02-17 16:50:55 +0100

    * 1

--- ImageHunt/Migrations/20190204202338_AddIdentity_Context.cs
+++ ImageHunt/Migrations/20190204202338_AddIdentity_Context.cs
@@ -12,9 +12,9 @@ namespace ImageHunt.Migrations
                 name: "AspNetRoles",
                 columns: table => new
                 {
-                    Id = table.Column<string>(nullable: false),
-                    Name = table.Column<string>(nullable: true, unicode:false),
-                    NormalizedName = table.Column<string>(nullable: true, unicode:false),
+                    Id = table.Column<string>(type: "longtext", nullable: false),
+                    Name = table.Column<string>(type:"longtext", nullable: true, unicode:false),
+                    NormalizedName = table.Column<string>(type: "longtext", nullable: true, unicode:false),
                     ConcurrencyStamp = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
@@ -26,15 +26,15 @@ namespace ImageHunt.Migrations
                 name: "AspNetUsers",
                 columns: table => new
                 {
-                    Id = table.Column<string>(nullable: false),
-                    UserName = table.Column<string>(nullable: true, unicode:false),
-                    NormalizedUserName = table.Column<string>(nullable: true, unicode:false),
-                    Email = table.Column<string>(nullable: true, unicode:false),
-                    NormalizedEmail = table.Column<string>(nullable: true, unicode:false),
+                    Id = table.Column<string>(type: "longtext", nullable: false),
+                    UserName = table.Column<string>(type: "longtext", nullable: true, unicode:false),
+                    NormalizedUserName = table.Column<string>(type: "longtext", nullable: true, unicode:false),
+                    Email = table.Column<string>(type: "longtext", nullable: true, unicode:false),
+                    NormalizedEmail = table.Column<string>(type: "longtext", nullable: true, unicode:false),
                     EmailConfirmed = table.Column<bool>(nullable: false),
-                    PasswordHash = table.Column<string>(nullable: true),
-                    SecurityStamp = table.Column<string>(nullable: true),
-                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<string>(type: "longtext", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "longtext", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "longtext", nullable: true),
                     PhoneNumber = table.Column<string>(nullable: true),
                     PhoneNumberConfirmed = table.Column<bool>(nullable: false),
                     TwoFactorEnabled = table.Column<bool>(nullable: false),
