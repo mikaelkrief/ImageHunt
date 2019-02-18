86f57822e0379aeed40057a979c89dc46a7b6202
nimbusparis <mandrillon@gmail.com>
2019-02-18 21:26:01 +0100

    * Add method to duplicate node

--- ImageHunt/Model/NodeFactory.cs
+++ ImageHunt/Model/NodeFactory.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ImageHuntCore.Model.Node;
 using ImageHuntWebServiceClient.Responses;
 
@@ -31,6 +32,47 @@ namespace ImageHunt.Model
           return new WaypointNode();
       }
       return null;
+    }
+
+    public static Node DuplicateNode(Node orgNode)
+    {
+      var newNode = CreateNode(orgNode.NodeType);
+      newNode.Latitude = orgNode.Latitude;
+      newNode.Longitude = orgNode.Longitude;
+      newNode.Name = orgNode.Name;
+      newNode.Points = orgNode.Points;
+      switch (orgNode.NodeType)
+      {
+        case NodeResponse.FirstNodeType:
+          ((FirstNode) newNode).Password = ((FirstNode) orgNode).Password;
+          break;
+        case NodeResponse.ObjectNodeType:
+          ((ObjectNode) newNode).Action = ((ObjectNode) orgNode).Action;
+          break;
+        case NodeResponse.PictureNodeType:
+          ((PictureNode) newNode).Image = ((PictureNode) orgNode).Image;
+          break;
+        case NodeResponse.QuestionNodeType:
+          ((QuestionNode) newNode).Question = ((QuestionNode) orgNode).Question;
+          ((QuestionNode) newNode).Answer = ((QuestionNode) orgNode).Answer;
+          break;
+        case NodeResponse.TimerNodeType:
+          ((TimerNode) newNode).Delay = ((TimerNode) orgNode).Delay;
+          break;
+        case NodeResponse.HiddenNodeType:
+          ((HiddenNode) newNode).LocationHint = ((HiddenNode) orgNode).LocationHint;
+          break;
+        case NodeResponse.ChoiceNodeType:
+          ((ChoiceNode) newNode).Choice = ((ChoiceNode) orgNode).Choice;
+          ((ChoiceNode)newNode).Answers = new List<Answer>();
+          ((ChoiceNode)orgNode).Answers.ForEach(a=>((ChoiceNode)newNode).Answers.Add(new Answer(){Response = a.Response, Correct = a.Correct})); 
+          break;
+        case NodeResponse.BonusNodeType:
+          ((BonusNode) newNode).Location = ((BonusNode) orgNode).Location;
+          ((BonusNode) newNode).BonusType = ((BonusNode) orgNode).BonusType;
+          break;
+      }
+      return newNode;
     }
   }
 }
--- ImageHuntTest/Model/NodeFactoryTest.cs
+++ ImageHuntTest/Model/NodeFactoryTest.cs
@@ -1,29 +1,162 @@
 ﻿using System;
+using System.Collections.Generic;
 using ImageHunt.Model;
+using ImageHuntCore.Model;
 using ImageHuntCore.Model.Node;
 using ImageHuntWebServiceClient.Responses;
 using NFluent;
+using NFluent.Extensions;
 using Xunit;
 
 namespace ImageHuntTest.Model
 {
-  public class NodeFactoryTest
-  {
-    [Theory]
-    [InlineData(NodeResponse.TimerNodeType, typeof(TimerNode))]
-    [InlineData(NodeResponse.ObjectNodeType, typeof(ObjectNode))]
-    [InlineData(NodeResponse.ChoiceNodeType, typeof(ChoiceNode))]
-    [InlineData(NodeResponse.FirstNodeType, typeof(FirstNode))]
-    [InlineData(NodeResponse.LastNodeType, typeof(LastNode))]
-    [InlineData(NodeResponse.PictureNodeType, typeof(PictureNode))]
-    public void NodeFromNodeType(string nodeName, Type expectedType)
+    public class NodeFactoryTest
     {
-      // Arrange
-      var nodeType = nodeName;
-      // Act
-      var node = NodeFactory.CreateNode(nodeType);
-      // Assert
-      Check.That(node.GetType()).Equals(expectedType);
+        [Theory]
+        [InlineData(NodeResponse.TimerNodeType, typeof(TimerNode))]
+        [InlineData(NodeResponse.ObjectNodeType, typeof(ObjectNode))]
+        [InlineData(NodeResponse.ChoiceNodeType, typeof(ChoiceNode))]
+        [InlineData(NodeResponse.FirstNodeType, typeof(FirstNode))]
+        [InlineData(NodeResponse.LastNodeType, typeof(LastNode))]
+        [InlineData(NodeResponse.PictureNodeType, typeof(PictureNode))]
+        public void NodeFromNodeType(string nodeName, Type expectedType)
+        {
+            // Arrange
+            var nodeType = nodeName;
+            // Act
+            var node = NodeFactory.CreateNode(nodeType);
+            // Assert
+            Check.That(node.GetType()).Equals(expectedType);
+        }
+
+        [Fact]
+        public void Should_Duplicate_ObjectNode()
+        {
+            // Arrange
+            var node = new ObjectNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, Action = "Action" };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_FirstNode()
+        {
+            // Arrange
+            var node = new FirstNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, Password = "Action" };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_TimerNode()
+        {
+            // Arrange
+            var node = new TimerNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, Delay = 56 };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_QuestionNode()
+        {
+            // Arrange
+            var node = new QuestionNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, Question = "Question", Answer = "Answer" };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_PictureNode()
+        {
+            // Arrange
+            var node = new PictureNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, Image = new Picture() };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_HiddenNode()
+        {
+            // Arrange
+            var node = new HiddenNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, LocationHint = "Location" };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_BonusNode()
+        {
+            // Arrange
+            var node = new BonusNode() { Latitude = 5, Longitude = 6, Name = "toto", Points = 56, Location = "Location", BonusType = BonusNode.BONUS_TYPE.Points_x2 };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
+        [Fact]
+        public void Should_Duplicate_ChoiceNode()
+        {
+            // Arrange
+            var node = new ChoiceNode()
+            {
+                Latitude = 5,
+                Longitude = 6,
+                Name = "toto",
+                Points = 56,
+                Choice = "Choice",
+                Answers = new List<Answer>(){
+              new Answer()
+          {
+              Response = "Response1", Correct = true
+          },
+              new Answer()
+              {
+                  Response = "Response2", Correct = false,
+              }
+          }
+            };
+            // Act
+            var newNode = NodeFactory.DuplicateNode(node);
+            // Assert
+            Check.That(newNode.Latitude).Equals(node.Latitude);
+            Check.That(newNode.Longitude).Equals(node.Longitude);
+            Check.That(newNode.Name).Equals(node.Name);
+            Check.That(newNode.Points).Equals(node.Points);
+            Check.That(newNode).HasFieldsWithSameValues(node);
+        }
     }
-  }
 }
