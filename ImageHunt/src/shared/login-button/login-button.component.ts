e8e04ddb28b027615de7091a11cabbfd0ce567b3
nimbusparis <mandrillon@gmail.com>
2019-02-19 07:45:45 +0100

    * Allow user to register from nav-menu

--- ImageHunt/src/shared/login-button/login-button.component.ts
+++ ImageHunt/src/shared/login-button/login-button.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap';
 import { LoginFormComponent } from '../../account/login-form/login-form.component';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { RegistrationFormComponent } from '../../account/registration-form/registration-form.component';
 
 @Component({
     selector: 'login-button',
@@ -17,6 +18,9 @@ export class LoginButtonComponent {
   }
   login() {
     this.modalRef = this._modalService.show(LoginFormComponent, { ignoreBackdropClick: true });
+  }
+  register() {
+    this.modalRef = this._modalService.show(RegistrationFormComponent, { ignoreBackdropClick: true });
   }
   logout() {
     localStorage.removeItem('authToken');
